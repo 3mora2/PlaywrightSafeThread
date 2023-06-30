235424b93a77c23432ad85ea9d5e730a7b486078
3mora2 <66757189+3mora2@users.noreply.github.com>
2023-06-30 22:12:11 +0300

    add code

---
+++ PlaywrightSafeThread/__init__.py
@@ -0,0 +1,1 @@
+from PlaywrightSafeThread.PlaywrightSafeThread.browser.threadsafe_browser import ThreadsafeBrowser
---
+++ PlaywrightSafeThread/test.py
@@ -0,0 +1,7 @@
+from PlaywrightSafeThread.PlaywrightSafeThread import ThreadsafeBrowser
+
+th = ThreadsafeBrowser(browser="chromium", channel="chrome", install=False, headless=False, no_viewport=True, bypass_csp=True)
+th.get("https://web.whatsapp.com/", wait_until="domcontentloaded")
+print(th.page_evaluate("""() => {return (typeof window.WAPI !== 'undefined' &&typeof window.Store !== 'undefined');}"""))
+th.page_wait_for_function("() => (window?.webpackChunkwhatsapp_web_client?.length || 0) > 3")
+
